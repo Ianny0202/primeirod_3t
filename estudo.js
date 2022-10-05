4d01b718c51fef03f29d84b428dab7264518c56c
Ianny0202 <110680387+Ianny0202@users.noreply.github.com>
2022-10-05 11:08:49 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -10,5 +10,23 @@ function escreva(){
   document.write(tabuada + " x 6 = "+(tabuada*6)+"<br>");
   document.write(tabuada + " x 7 = "+(tabuada*7)+"<br>");
   document.write(tabuada + " x 8 = "+(tabuada*8)+"<br>");
+  document.write(tabuada + " x 9 = "+(tabuada*9)+"<br>");
+  document.write(tabuada + " x 10 = "+(tabuada*10)+"<br>");
 }
 
+function repete(){
+
+  for(let i = 1; i <= 10; i++){
+    document.write("Tabuada do " + i + "<br>");
+    for(let j =1; j <= 10; j++){
+    document.write(i + " x " + j + " = "+(j*i)+"<br>");
+    }
+    document.write("<br>");
+  }
+}
+
+let lista = ["Banana","Uva","Abacate","Tomate"];
+
+fucntion MinhaLista(){
+  document.write(lista [3])
+}
